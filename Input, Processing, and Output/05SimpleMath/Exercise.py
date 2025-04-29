4bc4097b60a49bf5531da569af2abc21689dcfb2
Yuvraj-Singh-0505 <126414846+Yuvraj-Singh-0505@users.noreply.github.com>
2025-04-29 18:16:07 +0530

    Update Exercise.py

--- Input, Processing, and Output/05SimpleMath/Exercise.py
+++ Input, Processing, and Output/05SimpleMath/Exercise.py
@@ -1,10 +1,7 @@
-# Prompting the user for two numbers
 num1 = input("What is the first number? ")
 num2 = input("What is the second number? ")
 
-# Converting input strings to numbers
 num1 = float(num1)
 num2 = float(num2)
 
-# Printing the results with a single output statement
 print(f"{num1} + {num2} = {num1 + num2}\n{num1} - {num2} = {num1 - num2}\n{num1} * {num2} = {num1 * num2}\n{num1} / {num2} = {num1 / num2}")
