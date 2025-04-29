04832f3b6ff91c77a7705e168d63b823c8bb5e87
Yuvraj-Singh-0505 <126414846+Yuvraj-Singh-0505@users.noreply.github.com>
2025-04-29 18:15:31 +0530

    Update Exercise.py

--- Input, Processing, and Output/03PrintingQuotes/Exercise.py
+++ Input, Processing, and Output/03PrintingQuotes/Exercise.py
@@ -1,6 +1,4 @@
-# Prompting for the quote and the author
 quote = input("What is the quote? ")
 author = input("Who said it? ")
 
-# Displaying the output using string concatenation and escape characters
 print(author + " says, \"" + quote + "\"")
